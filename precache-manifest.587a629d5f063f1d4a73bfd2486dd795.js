422a4432ec76fa2b3b8acac59e57ca7fb9678c9c
Oksana <oksanabibik93@gmail.com>
2019-08-01 17:27:55 -0700

    add

---
+++ precache-manifest.587a629d5f063f1d4a73bfd2486dd795.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448030467153f951f4238259c7af17d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "044f7bb8d4f9d95a21e8",
+    "url": "/static/css/main.7aca53d4.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "044f7bb8d4f9d95a21e8",
+    "url": "/static/js/main.a7b82c22.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
